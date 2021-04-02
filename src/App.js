4bc8e95d04598d3afc0761e63619bd961c22e3ce
andreiamartinho82 <77107392+andreiamartinho82@users.noreply.github.com>
2021-04-02 14:10:49 +0100

    added github repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
      <h1>Weather App</h1>
+     <footer>
+     <a href="https://github.com/andreiamartinho82/weather-react-final-project" target="_blank">Coded by Andreia Martinho - GitHub Open-Sourced</a>
+    </footer>
     </div>
   );
 }
