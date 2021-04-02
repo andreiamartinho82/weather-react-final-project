4aab04faf36e3828957e23c7bc10621874df2711
andreiamartinho82 <77107392+andreiamartinho82@users.noreply.github.com>
2021-04-02 17:55:58 +0100

    Added CSS - Basic Styling
    basic styling  of the page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <div className="container">
     
      <Weather />
-     <footer>
-     <a href="https://github.com/andreiamartinho82/weather-react-final-project" target="_blank">Coded by Andreia Martinho - GitHub Open-Sourced</a>
+     <footer>Coded by Andreia Martinho -
+
+     <a href="https://github.com/andreiamartinho82/weather-react-final-project" target="_blank"> GitHub Open-Sourced</a>
     
     </footer>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -8,10 +8,10 @@ export default function Weather(){
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Search for a city" className="form-control"/>
+                <input type="search" placeholder="Search for a city" className="form-control" autoFocus="on"/>
                </div>
                <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary"/>
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
             </div>
             </div>
             </form>
@@ -24,10 +24,10 @@ export default function Weather(){
         Mostly Cloudy
     </li>
 </ul>
-<div className="row">
+<div className="row" mt-3>
 <div className="col-6">
 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"/>
-6ºC
+<span className="temperature">6</span><span className="units">ºC</span>
 </div>
 <div className="col-6">
     <ul>
