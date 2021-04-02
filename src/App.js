d7856a33658a76b911bf144682aa6a3b060743c7
andreiamartinho82 <77107392+andreiamartinho82@users.noreply.github.com>
2021-04-02 14:25:49 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1>
      <footer>
      <a href="https://github.com/andreiamartinho82/weather-react-final-project" target="_blank">Coded by Andreia Martinho - GitHub Open-Sourced</a>
+    
     </footer>
+    </div>
     </div>
   );
 }
