d60954873cd7d36ce15b49bd3c8a61ba0d7431b5
andreiamartinho82 <77107392+andreiamartinho82@users.noreply.github.com>
2021-04-03 21:23:57 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -36,7 +36,7 @@ export default function Weather(props){
     function search(){
         
   const apiKey="0ede9060c9da0e5d78059f6085416d96";
-  let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
 
     }
